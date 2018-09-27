286ca7b154b4057fc481a181af0ca4f08eb199ef
Daniel <dds2130@rit.edu>
2018-09-27 13:26:27 -0400

    added more to home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -13,8 +13,12 @@ export default function Home() {
         <span>Built by fans for fans.</span>
         <br /><br />
         <div>Made with <span className="heart">♥</span> by
-        <br />Danude Sandstorm
-        <br />Chiodosin1
+          <br />Danude Sandstorm
+          <br />Card database contributed by Chiodosin1
+          <div>We were unsatisfied with the options on how to search for cards. I took the design of the old Chaotic website and added my own modernizations.
+            <br />With an extensive lists of search options in the <a href="/collection">collection</a>, you'll find deck building mores streamlined than ever before.
+            <br />Chaotic is full of rich lore, but unfortunately the best database of official lore disapeared when the <a href="/portal">Portal to Perim</a> disapeared along with the site. You can again explore the official lore and information of Creatures!
+          </div>
         </div>
         <br />
         <div>Do you like the site? You can donate to support it!</div>
